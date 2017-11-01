395c898744583af57fa87ed717fa9c59af84b60a
sd3ntato <valeriomariani52@gmail.com>
2017-11-01 22:42:40 +0100

    [UPDATE]revisionato il codice

--- Bot_MKSpace.py
+++ Bot_MKSpace.py
@@ -4,102 +4,67 @@ import string
 import re
 from pprint import pprint
 
-Statomacchina = 0
 
 
 def operazione(a,b,op):
+	try:
+		if op == 1:
+				risposta = ("L addizione tra i due numeri e: %s" % (int(a.split()[0]) + int(b.split()[0])))
+		elif op == 2:
+				risposta = ("L addizione tra i due numeri e: %s" % (int(a.split()[0]) - int(b.split()[0])))
+		elif op == 3:
+				risposta = ("L addizione tra i due numeri e: %s" % (int(a.split()[0]) * int(b.split()[0])))
+		elif op == 4:
+				risposta = ("L addizione tra i due numeri e: %s" % (int(a.split()[0]) / int(b.split()[0])))
 
-	if op == 1:
-		try:
-			a1 = int(a.split()[0])
-			b1 = int(b.split()[0])
-			c = a1 + b1
-			risposta = ("L addizione tra i due numeri e: %s" % c)
-
-		except ValueError:
-			risposta = ("Sei un cretino! che razza di numeri sarebbero questi?!")
-	elif op == 2:
-		try:
-			a1 = int(a.split()[0])
-			b1 = int(b.split()[0])
-			c = a1 - b1
-			risposta = ("L addizione tra i due numeri e: %s" % c)
-
-		except ValueError:
-			risposta = ("Sei un cretino! che razza di numeri sarebbero questi?!")
-	elif op == 3:
-		try:
-			a1 = int(a.split()[0])
-			b1 = int(b.split()[0])
-			c = a1 * b1
-			risposta = ("L addizione tra i due numeri e: %s" % c)
-
-		except ValueError:
-			risposta = ("Sei un cretino! che razza di numeri sarebbero questi?!")
-	elif op == 4:
-		try:
-			a1 = float(a.split()[0])
-			b1 = float(b.split()[0])
-			c = a1 / b1
-			risposta = ("L addizione tra i due numeri e: %s" % c)
-
-		except ValueError:
+	except ValueError:
 			risposta = ("Sei un cretino! che razza di numeri sarebbero questi?!")
 	return risposta
 
+Statomacchina = 0
 def handle(msg):
 	content_type, chat_type, chat_id = telepot.glance(msg)
-
 	global Statomacchina
 	ver1 = 0
-
-
 	chat_id = msg['chat']['id']
 	command_input = msg['text']
-
 	print(chat_id, content_type, chat_type)
-
-	response = bot.getUpdates()
 	print "msg:"
 	pprint(msg)
 	if Statomacchina == 0:
 		if command_input == '/risolvi' or command_input == '/risolvi@CompitatorBot':
-			risolvi_str = "Illustrami l'operazione che vorresti risolvere "
-			bot.sendMessage(chat_id, risolvi_str)
+			 
 			Statomacchina = 1
+			risolvi_str = "Illustrami l'operazione che vorresti risolvere" 
+			bot.sendMessage(chat_id, risolvi_str)
+			
 		else:
 			bot.sendMessage(chat_id, "Non mi conosci, puoi chiedermi soltanto /risolvi")
-
+	
 	elif Statomacchina == 1:
 
 		#addizione
-		check = command_input.find('+')
-		if check != -1 :
-				ver1 = 1
+		if command_input.find('+') != -1 :
 				a, b = command_input.split('+')
-				risposta=operazione(a,b,ver1)
-
+				risposta=operazione(a,b,1)
+				ver1 = 1
 
 		#sottrazione
-		check = command_input.find('-')
-		if check != -1:
-				ver1 = 2
+		if command_input.find('-') != -1 :
 				a, b = command_input.split('-')
-				risposta=operazione(a,b,ver1)
+				risposta=operazione(a,b,2)
+				ver1 = 1
 
 		#moltiplicazione
-		check = command_input.find('*')
-		if check != -1:
-				ver1 = 3
+		if command_input.find('*') != -1 :
 				a, b = command_input.split('*')
-				risposta=operazione(a,b,ver1)
+				risposta=operazione(a,b,3)
+				ver1 = 1
 		#divisione
-		check = command_input.find('/')
-		if check != -1:
-
-				ver1 = 4
+		if command_input.find('/') != -1 :
 				a, b = command_input.split('/')
-				risposta=operazione(a,b,ver1)
+				risposta=operazione(a,b,4)
+				ver1 = 1
 
 
 
@@ -111,7 +76,7 @@ def handle(msg):
 		bot.sendMessage(chat_id, risposta)
 
 
-bot = telepot.Bot('token')
+bot = telepot.Bot('487365523:AAGyY9FrLZK_Mo42MQwTCMgzd_L6KVit0bI')
 
 bot.message_loop(handle)
 
